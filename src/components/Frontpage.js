28851940ff642f87990e8e69a1fdd61d268460b7
Topias <topias.jappila@gmail.com>
2017-03-29 22:30:15 +0300

    added frontpage

---
+++ src/components/Frontpage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import Search from './Search'
+
+class Frontpage extends Component {
+
+  render() {
+    return (
+      <p>Etusivu</p>
+    );
+  }
+}
+
+export default Frontpage;
